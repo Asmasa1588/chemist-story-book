4919afab76cadd908709a22a4ce798f4812262d7
Asma <as1588ma@gmail.com>
2021-12-27 11:24:13 -0500

    implement client login form

--- client/index.js
+++ client/index.js
@@ -1,1 +1,11 @@
 console.log("Jupiter is bothering Gypsy");
+function onSubmit(event) {
+  console.log(event);
+  const username = event.target[0].value;
+
+  const password = event.target[1].value;
+
+  console.log(username, password);
+
+  return false;
+}
