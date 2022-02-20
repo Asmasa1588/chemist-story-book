76b74c0ac331814cde5f2f10cbcc086085142b0d
Asma <as1588ma@gmail.com>
2022-02-19 21:20:23 -0500

    add a seed file

---
+++ api/chemist-storybook-seed.sql
@@ -0,0 +1,29 @@
+
+DROP DATABASE IF EXISTS chemist_storybook_db;
+
+CREATE DATABASE chemist_storybook_db;
+-- Always selecting the correct database on line #6,otherwise 
+-- mysql will not recognize for which database we are sending quesries.
+USE chemist_storybook_db;
+
+CREATE TABLE users(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+CREATE TABLE chemist_stories(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    content VARCHAR(50) NOT NULL,
+    authorID int,
+    FOREIGN KEY (authorID) REFERENCES users(id)
+    
+);
+
+INSERT INTO users(username, password)
+VALUES ('jupiter', '123');
+
+INSERT INTO users(username, password)
+VALUES ('gypsy', '456');
+
+SELECT * FROM users;
